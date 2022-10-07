ac0c60647fd11a40a633a4c2c0505f4bf6e3da35
Moses Bomera <mosesbomera@gmail.com>
2022-10-07 04:55:19 +0300

    Add time binning feature

--- msgc/features.py
+++ msgc/features.py
@@ -87,3 +87,42 @@ def preprocess_sample(df):
     return df
 
 # FEATURE ENGINEERING
+
+def int_per_timebin(df):
+
+    """
+    Transforms dataset to take the preprocessed max abundance for each
+    time range for each m/z value
+
+    Args:
+        df: dataframe to transform
+
+    Returns:
+        transformed dataframe
+    """
+    # Create a series of time bins
+    timerange = pd.interval_range(start=0, end=25, freq=0.5)
+
+    # Make dataframe with rows that are combinations of all temperature bins and all m/z values
+    allcombs = list(itertools.product(timerange, [*range(0, 350)]))
+
+    allcombs_df = pd.DataFrame(allcombs, columns=["time_bin", "rounded_mass"])
+
+    # Bin times
+    df["time_bin"] = pd.cut(df["time"], bins=timerange)
+
+    # Combine with a list of all time bin-m/z value combinations
+    df = pd.merge(allcombs_df, df, on=["time_bin", "rounded_mass"], how="left")
+
+    # Aggregate to time bin level to find max
+    df = df.groupby(["time_bin", "rounded_mass"]).max("int_minsub_scaled").reset_index()
+
+    # Fill in 0 for intensity values without information
+    df = df.replace(np.nan, 0)
+
+    # Reshape so each row is a single sample
+    df = df.pivot_table(
+        columns=["rounded_mass", "time_bin"], values=["int_minsub_scaled"]
+    )
+
+    return df
